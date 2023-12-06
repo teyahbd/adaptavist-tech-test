c549177f11169e9bb074db0c694c25f1af2be023
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-12-06 13:41:34 +0000

    feat: add array sorting util function

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,24 @@
-export default function cleanWord(word: string): string {
+import { WordCountObject, isWordCountObjectArray } from "./types";
+
+export function cleanWord(word: string): string {
   if (!word) throw new Error("No word provided");
 
   if (typeof word !== "string") throw new Error("Incorrect input type");
 
   return word.replace(/[^a-zA-Z]/g, "").toLowerCase();
+}
+
+export function sortWordCountArrayAlphabetically(
+  wordCountArray: WordCountObject[],
+) {
+  if (!isWordCountObjectArray(wordCountArray))
+    throw new Error("Incorrect input type");
+  const sortedArray = wordCountArray.map((obj) => ({ ...obj }));
+
+  return sortedArray.sort((a, b) => {
+    const keyA = Object.keys(a)[0];
+    const keyB = Object.keys(b)[0];
+
+    return keyA < keyB ? -1 : keyA > keyB ? 1 : 0;
+  });
 }
