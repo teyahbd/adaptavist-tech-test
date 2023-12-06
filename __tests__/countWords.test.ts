68a4cc8d83408dcdb1cb29596125fc8f9600a968
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-12-06 19:43:19 +0000

    test: add passing tests for word counting function

---
+++ __tests__/countWords.test.ts
@@ -0,0 +1,49 @@
+import countWords from "../countWords";
+
+describe("countWords()", () => {
+  it("returns an empty object if passed an empty array", () => {
+    const result = countWords([]);
+
+    expect(result).toEqual({});
+  });
+  it("returns correct count for array containing one word", () => {
+    const input = ["armadillo"];
+    const expected = { armadillo: 1 };
+
+    const result = countWords(input);
+
+    expect(result).toEqual(expected);
+  });
+  it("returns correct count for array containing two of the same word", () => {
+    const inputA = ["armadillo", "armadillo"];
+    const expectedA = { armadillo: 2 };
+
+    const resultA = countWords(inputA);
+
+    expect(resultA).toEqual(expectedA);
+  });
+  it("returns correct count for an array of two different words", () => {
+    const input = ["armadillo", "zebra"];
+    const expected = { armadillo: 1, zebra: 1 };
+
+    const result = countWords(input);
+
+    expect(result).toEqual(expected);
+  });
+  it("counts all versions of a word in the array", () => {
+    const input = [" armadillo", "Armadillo!", "zebra ", "armaDIllo", ".ZEBRA"];
+    const expected = { armadillo: 3, zebra: 2 };
+
+    const result = countWords(input);
+
+    expect(result).toEqual(expected);
+  });
+  it("ERROR: throws error if not passed argument", () => {
+    expect(countWords).toThrow(Error("No input provided"));
+  });
+  it("ERROR: throws error if not passed string array as argument", () => {
+    expect(() => countWords("armadillo" as any)).toThrow(
+      Error("Incorrect input type"),
+    );
+  });
+});
