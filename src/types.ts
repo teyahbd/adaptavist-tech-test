7d39619500b6b4d49161192c215384a9bd6839dc
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-12-06 13:19:08 +0000

    feat: add type and typeguards for word count object and array

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface WordCountObject {
+  [key: string]: number;
+}
+
+export function isWordCountObject(arg: any): arg is WordCountObject {
+  return (
+    typeof arg === "object" &&
+    arg !== null &&
+    Object.values(arg).every((value) => typeof value === "number") &&
+    Object.keys(arg).length === 1
+  );
+}
+export function isWordCountObjectArray(arg: any): arg is WordCountObject[] {
+  return Array.isArray(arg) && arg.every((obj: any) => isWordCountObject(obj));
+}
